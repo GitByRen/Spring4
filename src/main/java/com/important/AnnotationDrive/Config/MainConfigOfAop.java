3d78326c7d1cf4e0d39b47fdbdddd3fccbe80fb5
GitByRen <1402443141@qq.com>
2018-09-01 21:59:31 +0800

    tx

--- src/main/java/com/important/AnnotationDrive/Config/MainConfigOfAop.java
+++ src/main/java/com/important/AnnotationDrive/Config/MainConfigOfAop.java
@@ -1,15 +1,31 @@
 package com.important.AnnotationDrive.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.important.AnnotationDrive.aop.LogAspects;
+import com.important.AnnotationDrive.aop.MathCalculator;
 
 /**
  * @EnableAspectJAutoProxy:开启基于注解的aop模式[<aop:aspectj-autoproxy></aop:aspectj-autoproxy>]
  * 
- * JoinPoint一定要出现在参数表的第一位
+ * 																						JoinPoint一定要出现在参数表的第一位
  */
 @EnableAspectJAutoProxy
 @Configuration
 public class MainConfigOfAop {
+
+	// 业务逻辑类加入容器中
+	@Bean
+	public MathCalculator calculator() {
+		return new MathCalculator();
+	}
+
+	// 切面类加入到容器中
+	@Bean
+	public LogAspects logAspects() {
+		return new LogAspects();
+	}
 
 }
