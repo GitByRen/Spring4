5bb05f318e75dda940a2adb1c1bbe045d9172862
GitByRen <1402443141@qq.com>
2019-07-14 18:53:54 +0800

    annotation

--- src/main/java/com/important/AnnotationDrive/Config/MainConfig.java
+++ src/main/java/com/important/AnnotationDrive/Config/MainConfig.java
@@ -3,11 +3,14 @@ package com.important.AnnotationDrive.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
-
-import com.important.AnnotationDrive.bean.Person;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.important.AnnotationDrive.Annotation.TestService;
+import com.important.AnnotationDrive.bean.Person;
 
 // 指定这是一个配置类
 @Configuration
@@ -20,8 +23,8 @@ import org.springframework.context.annotation.FilterType;
 		@Filter(type = FilterType.ANNOTATION, classes = { Controller.class, Service.class }) })
 */
 @ComponentScan(value = "com.important.AnnotationDrive.Annotation", includeFilters = {
-//		@Filter(type = FilterType.ANNOTATION, classes = { Controller.class }),
-//		@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { TestService.class }),
+		@Filter(type = FilterType.ANNOTATION, classes = { Controller.class, Service.class, Repository.class }),
+		@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { TestService.class }),
 		@Filter(type = FilterType.CUSTOM, classes = { MyTypeFilter.class })}, useDefaultFilters = false)
 /**
  * FilterType：
